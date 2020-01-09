afacbbe7a26b4e11c2bd83beed56e95796a67bd2
PaulElcampeon <pauloladele@live.co.uk>
2020-01-09 19:13:07 +0000

    Removed editcredential middleware.

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import addCredentialsMiddleware from './middleware/addCredentialsMiddleware';
-import editCredentialsMiddleware from './middleware/editCredentialsMiddleware';
 import removeCredentialMiddleware from './middleware/removeCredentialMiddleware';
 import loginMiddleware from './middleware/loginMiddleware';
 import registrationMiddleware from './middleware/registrationMiddleware';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
-    applyMiddleware(addCredentialsMiddleware, editCredentialsMiddleware, removeCredentialMiddleware, loginMiddleware, registrationMiddleware)
+    applyMiddleware(addCredentialsMiddleware, removeCredentialMiddleware, loginMiddleware, registrationMiddleware)
 ));
 
 export default store;
