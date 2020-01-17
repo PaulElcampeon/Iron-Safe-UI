00fcbeeba11a5c0484855db30eeb85bbe390ef6c
PaulElcampeon <pauloladele@live.co.uk>
2020-01-17 19:04:34 +0000

    Added validation for credetials.

--- src/store/middleware/addCredentialsMiddleware.js
+++ src/store/middleware/addCredentialsMiddleware.js
@@ -1,24 +1,28 @@
 import { ADD_CREDENTIAL_DATA_BASE, updateMessage, addCredentialToStore } from '../actions/index';
+import CredentialUtils from '../../utils/credentialUtils';
 import { addCredential } from '../../comms/commsService'
 import store from '../index';
-
 export const addCredentialMiddleware = (state) => (next) => (action) => {
     switch (action.type) {
         case ADD_CREDENTIAL_DATA_BASE:
-            addCredential(action.credential, store.getState().token)
-            .then(res => res.json())
-            .then(response => {
-                    if (response) { 
-                        store.dispatch(updateMessage('Credential was added'))
-                        store.dispatch(addCredentialToStore(action.credential))
-                    } else {
-                        store.dispatch(updateMessage('Credential was not added'))
-                    }
-             })
-            .catch(error => {
-                console.log(error)
-                store.dispatch(updateMessage("Something went wrong"))
-            })
+            if (CredentialUtils.isCredentialValid(action.credential.value) && CredentialUtils.isCredentialValid(action.credential.key)) {
+                addCredential(action.credential, store.getState().token)
+                .then(res => res.json())
+                .then(response => {
+                        if (response) { 
+                            store.dispatch(updateMessage('Credential was added'))
+                            store.dispatch(addCredentialToStore(action.credential))
+                        } else {
+                            store.dispatch(updateMessage('Credential was not added'))
+                        }
+                })
+                .catch(error => {
+                    console.log(error)
+                    store.dispatch(updateMessage("Something went wrong"))
+                })
+            } else {
+                store.dispatch(updateMessage('Credentials were invalid \n Credentials must not contain empty spaces \n Credentials must not exceed more than 20 characters'))
+            }
             break;
         default:
             next(action);
